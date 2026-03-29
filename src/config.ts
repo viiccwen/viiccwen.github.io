1b7c6b8832a17beef8cfd46b35bccac5ba4e14d5
viiccwen <viiccwen@gmail.com>
2026-03-29 14:37:04 +0800

    Site Updated

--- src/config.ts
+++ src/config.ts
@@ -42,7 +42,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/vicwen.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/vicwen.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Vic Wen",
 	bio: "Software Engineer",
 	links: [
