d70988b4c13f46bb68dbe4192b1a20fa4ca1033d
viiccwen <viiccwen@gmail.com>
2026-05-02 22:46:54 +0800

    fix: resolve biome quality issues

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -85,8 +85,8 @@ export async function getTagList(
 	});
 
 	const countMap: { [key: string]: number } = {};
-	allBlogPosts.map((post: { data: { tags: string[] } }) => {
-		post.data.tags.map((tag: string) => {
+	allBlogPosts.forEach((post: { data: { tags: string[] } }) => {
+		post.data.tags.forEach((tag: string) => {
 			if (!countMap[tag]) countMap[tag] = 0;
 			countMap[tag]++;
 		});
@@ -117,7 +117,7 @@ export async function getCategoryList(
 		return import.meta.env.PROD ? data.draft !== true : true;
 	});
 	const count: { [key: string]: number } = {};
-	allBlogPosts.map((post: { data: { category: string | null } }) => {
+	allBlogPosts.forEach((post: { data: { category: string | null } }) => {
 		if (!post.data.category) {
 			const ucKey = i18n(I18nKey.uncategorized, locale);
 			count[ucKey] = count[ucKey] ? count[ucKey] + 1 : 1;
